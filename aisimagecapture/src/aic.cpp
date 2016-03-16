5544c71e0206910358baefd9d5b7f63563ec6f24
Arul selvam <arul@robo17.informatik.uni-bonn.de>
2016-03-16 17:58:46 +0100

    Almost done. But have to check the double key press issue

--- aisimagecapture/src/aic.cpp
+++ aisimagecapture/src/aic.cpp
@@ -7,74 +7,92 @@
 #include <image_transport/image_transport.h>
 #include <memory>
 #include <boost/thread.hpp>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <termios.h>
+#include "boost/filesystem.hpp"
 
 std::unique_ptr<image_transport::ImageTransport> g_it;
 image_transport::Subscriber g_rgbSub;
 image_transport::Subscriber g_depthSub;
-
-void spinRGBthread()
-{
-	ros::spinOnce();
-}
-
-void spinDepththread()
-{
-	ros::spinOnce();
-}
-
+bool g_canWrite = false;
+boost::mutex g_lock;
+std::string g_imgpath ="/home/arul/arul/APCImages/Image" ;
+int g_imgCount = 0;
 void depthTorgb(const cv::Mat& float_img, cv::Mat& rgb_img){
   if(rgb_img.rows != float_img.rows || rgb_img.cols != float_img.cols){
 	  rgb_img = cv::Mat(float_img.size(), CV_8UC1);}
       cv::convertScaleAbs(float_img, rgb_img, 100, 0.0);
 }
-void rgbImageCallback(const sensor_msgs::ImageConstPtr& img)
+
+void syncCallback(const sensor_msgs::ImageConstPtr& rgb_img,const sensor_msgs::ImageConstPtr& depth_img)
 {
-	try
+	boost::mutex::scoped_lock lock(g_lock);
+	if (g_canWrite)
 	{
-		ROS_INFO_STREAM("Debug: In  RGB ImageCallback");
-		cv::Mat image = cv_bridge::toCvShare(img, "rgb8")->image;
-		cv::imwrite("testrgb.png",image);
-	}
-	catch (cv_bridge::Exception& e)
-	{
-		ROS_ERROR("Could not convert from '%s' to 'rgb8'.", img->encoding.c_str());
+		cv_bridge::CvImagePtr cv_ptr;
+		try
+		{
+			cv::Mat image = cv_bridge::toCvShare(rgb_img, "rgb8")->image;
+			std::stringstream ss;
+			ss << std::setw(3) << std::setfill('0') << g_imgCount++;
+
+			std::string filename = g_imgpath + ss.str()+ "/rgb.png";
+			boost::filesystem::path filePath ( g_imgpath + ss.str() );
+			boost::system::error_code returnedError;
+			boost::filesystem::create_directories( filePath, returnedError );
+			if (!returnedError)
+			{
+				std::cout<<"Writing image to "<<filePath <<std::endl;
+				cv::imwrite(filename,image);
+				cv_ptr = cv_bridge::toCvCopy(depth_img);
+				cv::Mat depth_float_img = cv_ptr->image;
+				cv::Mat depth_img;
+				depthTorgb(depth_float_img, depth_img);
+				filename = g_imgpath + ss.str()+ "/depth.png";
+				cv::imwrite(filename,depth_img);
+				g_canWrite = false;
+			}
+		}
+		catch (cv_bridge::Exception& e)
+		{
+			ROS_ERROR("Image save not successful  '%s' ", e.what());
+		}
 	}
 }
-void depthImageCallback(const sensor_msgs::ImageConstPtr& img)
+
+int getch()
 {
-	cv_bridge::CvImagePtr cv_ptr;
-	try
+  static struct termios oldt, newt;
+  tcgetattr( STDIN_FILENO, &oldt);           // save old settings
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON);                 // disable buffering
+  tcsetattr( STDIN_FILENO, TCSANOW, &newt);  // apply new settings
+  int c = getchar();  // read character (non-blocking)
+  tcsetattr( STDIN_FILENO, TCSANOW, &oldt);  // restore old settings
+  return c;
+}
+
+void keyboardCallback()
+{
+	std::cout<<"Press Enterkey twice	 to save topic "<<std::endl;
+	while (true)
 	{
-		ROS_INFO_STREAM("Debug: In  Depth ImageCallback");
-		cv_ptr = cv_bridge::toCvCopy(img);
-		//cv::imwrite("testDepth.png",image);
+		 if( getchar() == 32 || getchar() == '\n')
+		 {
+			 g_canWrite = true;
+		 }
 	}
-	catch (cv_bridge::Exception& e)
-	{
-		ROS_ERROR(" Failed Depth image subscription %s", e.what());
-		return;
-	}
-	 cv::Mat depth_float_img = cv_ptr->image;
-	 cv::Mat rgb_img;
-	 depthTorgb(depth_float_img, rgb_img);
-	 cv::imwrite("testdepth.png",rgb_img);
 }
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "ImageCapture");
 	ros::NodeHandle nh;
-	boost::thread spinRGB_thread(&spinRGBthread);
-	boost::thread spinDepth_thread(&spinDepththread);
-	ROS_INFO_STREAM("Debug: Node up");
-	g_it.reset(new image_transport::ImageTransport(nh));
-	g_rgbSub = g_it->subscribe("/camera/rgb/image_rect_color", 1, &rgbImageCallback);
-	g_depthSub = g_it->subscribe("/camera/depth/image_rect", 1, &depthImageCallback);
-	bool canSpin = true;
-	while (canSpin)
-	{
-		boost::thread spinRGB_thread(&spinRGBthread);
-		boost::thread spinDepth_thread(&spinDepththread);
-		canSpin = false;
-	}
-
+	message_filters::Subscriber<sensor_msgs::Image> rgb_sub(nh, "/camera/rgb/image_rect_color", 1);
+    message_filters::Subscriber<sensor_msgs::Image> depth_sub(nh, "/camera/depth/image_rect", 1);
+    message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::Image> sync(rgb_sub, depth_sub, 10);
+    sync.registerCallback(boost::bind(&syncCallback, _1, _2));
+    boost::thread keyboardThread(keyboardCallback);
+    ros::spin();
+    return 0;
 }
